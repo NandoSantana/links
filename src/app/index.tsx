8f51ca88e9a8bdea7d34bb050f86ef01cfe457cc
Fernando Santana <fernando.santana@cmore.pt>
2026-04-08 20:26:52 -0300

    initial files

---
+++ src/app/index.tsx
@@ -0,0 +1,11 @@
+import { Text } from "react-native";
+
+export default function Index() {
+    return(
+        <>
+            <Text>teste</Text>
+            <Text>No android passa fica na barrar</Text>
+        </>
+
+    );
+}
