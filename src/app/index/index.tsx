a6cc3413b8efb9824dfaa1c53e97f68f7736f53e
Fernando Santana <bh.carvalho@gmail.com>
2026-04-26 17:05:55 -0300

    utils

--- src/app/index/index.tsx
+++ src/app/index/index.tsx
@@ -3,6 +3,7 @@ import { Image, TouchableOpacity, View } from "react-native";
 import { styles } from "./styles";
 
 import { Category } from "@/components/category";
+import { categories } from "@/utils/categories";
 import { MaterialIcons } from "@expo/vector-icons";
 import { colors } from "../styles/colors";
 
@@ -15,10 +16,14 @@ export default function Index() {
                     <MaterialIcons name="add" size={32} color={colors.green[300]} />
                 </TouchableOpacity>
             </View>
-
-            <Category name="Projetos" icon="code" />
-            <Category name="Site" icon="language" />
-            <Category name="Video" icon="movie" /> 
+            <Category 
+                name={categories[0].name} 
+                icon={categories[0].icon}  
+                isSelected 
+            />
+            <Category name="Projetos" icon="code"  isSelected />
+            <Category name="Site" icon="language" isSelected={false} />
+            <Category name="Video" icon="movie" isSelected /> 
 
         </View>
     );
